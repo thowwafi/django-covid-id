7209b8c752322bf6add9f0bc7a6fdec4b765c282
alfiansyah27 <alfiansyah@baezeni.com>
2020-03-31 14:58:46 +0700

    updat request function

--- covid_api/views.py
+++ covid_api/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import requests
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
 
 # Create your views here.
 def index(request):
-    req = requests.get("https://indonesia-covid-19-api.now.sh/api/provinsi")
+    url = "https://indonesia-covid-19-api.now.sh/api/provinsi"
+    session = requests.Session()
+    retry = Retry(connect=3, backoff_factor=0.5)
+    adapter = HTTPAdapter(max_retries=retry)
+    session.mount('http://', adapter)
+    session.mount('https://', adapter)
+
+    req = session.get(url)
     print(req.status_code)
     data = req.json()
     data = data.get('data')
