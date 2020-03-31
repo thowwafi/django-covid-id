9551c849a3d74b80cd036acc8b1f99f68236ccca
alfiansyah27 <alfiansyah@baezeni.com>
2020-03-31 13:33:05 +0700

    get data from API, show it on dataTables

--- covid_api/views.py
+++ covid_api/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
 
 # Create your views here.
 def index(request):
-    context = {}
+    req = requests.get("https://indonesia-covid-19-api.now.sh/api/provinsi")
+    print(req.status_code)
+    data = req.json()
+    data = data.get('data')
+
+    context = {
+        'data': data
+        }
     return render(request, 'index.html', context)
